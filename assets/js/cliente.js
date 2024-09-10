2b6d48e8aaa09dce76ed61cbd7bc54fe9f5819be
Gamaliel7 <jacua2@yahoo.com.mx>
2024-09-10 01:15:06 -0600

    'Tarea2'

---
+++ assets/js/cliente.js
@@ -0,0 +1,20 @@
+/*function saludar(){
+    console.log('Diseño web 1')
+    //alert('Diseño web 1')
+    console.log(document);
+    console.log(document.getElementById('parrafo1').innerHTML)
+    
+    console.log( document.getElementById('parrafo1').getAttribute('style'))
+}
+
+function cambiarParrafo2(){
+    //console.log(document.getElementById('parrafo2').setHTMLUnsafe('FES Aragon UNAM'))
+    document.getElementById('parrafo2').innerHTML = 'FES Aragon UNAM'
+}
+
+function cambiarFondoBody(){
+    const colores = ['#FFFFFF', '#33FF57', '#2222FF', '#22FF11', '#FF2222']
+    const colorAlea = colores[Math.floor(Math.random() * 5)]
+    console.log(colorAlea)
+    document.getElementById('cuerpo').style.backgroundColor = colorAlea
+}*/
